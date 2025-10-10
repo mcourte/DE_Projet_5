d2342ea87795d91237ec027789158e4fec376f1f
Magali Courté <courte.magali@gmail.com>
2025-10-10 10:58:33 +0200

    MAJ flake8

--- clean_csv.py
+++ clean_csv.py
@@ -16,13 +16,13 @@ df = pd.read_csv(
 
 # NETTOYAGE DU CSV
 
-## NAME
+# NAME
 
-# Uniformiser les noms de patients : Prénom Nom 
+# Uniformiser les noms de patients : Prénom Nom
 # # + supprime les espaces inutiles en début ou fin de chaine de caractères
 df['Name'] = df['Name'].str.title().str.strip()
 
-# On peut voir également que certains patients ont des Mrs, Mr ou Dr devant. 
+# On peut voir également que certains patients ont des Mrs, Mr ou Dr devant.
 # Il faut les supprimer pour plus de cohérence
 # Liste des préfixes à supprimer
 prefixes = ['Dr ', 'Mr ', 'Mrs ', 'Ms ']
@@ -31,28 +31,29 @@ prefixes = ['Dr ', 'Mr ', 'Mrs ', 'Ms ']
 for prefix in prefixes:
     df['Name'] = df['Name'].str.replace(f'^{prefix}', '', regex=True, case=False)
 
-#regex : ^Dr : ça ne supprime le prefixe que si il est en début de chaine
-#case: false : insensible à la casse, supprime les prefixes qu'importe leur format DR, mRs ... 
+# regex : ^Dr : ça ne supprime le prefixe que si il est en début de chaine
+# case: false : insensible à la casse, supprime les prefixes qu'importe leur format DR, mRs ...
 
 # print(df['Name'].head())
 
 
-## DOCTOR
-#Uniformiser les noms des médecins
+# DOCTOR
+
+# Uniformiser les noms des médecins
 df['Doctor'] = df['Doctor'].str.title().str.strip()
 
 # print(df['Doctor'].head())
 
 
-## INSURANCE PROVIDER
-#Uniformiser les noms d'assurance en enlevant les ""
+# INSURANCE PROVIDER
+
+# Uniformiser les noms d'assurance en enlevant les ""
 df['Insurance Provider'] = df['Insurance Provider'].str.replace('"', '').str.strip()
 
 # print(df['Insurance Provider'].head())
 
 
-## BILLING AMOUNT
-
+# BILLING AMOUNT
 
 # Remplacer le point décimal par une virgule, et garder 2 chiffres après la virgule
 # .apply(lambda x: f"{x:.2f}") : applique à chaque valeur de la colonne un format à deux chiffres après la virgule
